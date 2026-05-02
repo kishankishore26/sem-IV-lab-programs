4f102d09dc61afed3541e81043c66ec33ef8766d
Kishan N Kishore <128124775+kishankishore26@users.noreply.github.com>
2026-05-02 11:18:00 +0530

    Added UNIX programs

--- UNIX_LAB/program3.sh
+++ UNIX_LAB/program3.sh
@@ -5,7 +5,7 @@ if [ $(expr $year % 4) -ne 0 ]; then
 	echo "$year is not a leap year"
 elif [ $(expr $year % 100) -ne 0 ]; then
 	echo "$year is a leap year"
-elif [ $(expr %year % 400) -eq 0 ]; then
+elif [ $(expr $year % 400) -eq 0 ]; then
 	echo "$year is a leap year"
 else
 	echo "$year is not a leap year"
